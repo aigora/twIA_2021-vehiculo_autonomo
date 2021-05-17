ce6f670138c4f03c0643eccada614e1661e0caa5
juanpablodemiguel <80361765+juanpablodemiguel@users.noreply.github.com>
2021-05-17 23:39:23 +0200

    Update ultrasonidos_buzzer.ino

--- Código/ultrasonidos_buzzer.ino
+++ Código/ultrasonidos_buzzer.ino
@@ -1,64 +1,69 @@
-
 #define TRIGGER 5
 #define ECHO 6
 #define BUZZER 9
- 
-void setup() {
 
-  pinMode(ECHO, INPUT);
+void setup() {
+  
+  Serial.begin(9600);
+  
+  pinMode(ECHO, INPUT);                               
   pinMode(TRIGGER, OUTPUT);
   pinMode(BUZZER, OUTPUT);
+  char cdistancia[7];
 }
  
-
 void loop() {
-  // Preparamos el sensor de ultrasonidos
-  iniciarTrig();
  
-  // Obtenemos la distancia
-  float distancia = ObtenerDist();
+  iniciarTrig();                                           // Preparamos el sensor de ultrasonidos
+ 
+  
+  float distancia = ObtenerDist();                         // Obtenemos la distancia
  
   if (distancia <= 60)
   {
-    //Activamos los pitidos
-    pitido(distancia);
+    
+    pitido(distancia);                                     //Activamos los pitidos
   } 
 }
 
-
-void pitido(float distancia)
+void pitido(int distancia,char cdistancia)
 {
-  //La frecuencia tiene una relación lineal con la distancia
-  float frecuencia = -7.5 * distancia + 900;
-  float duracion = 5 * distancia + 200;        //La duración también tiene una relación lineal con la distancia
+  sprintf(cdistancia, "%d", distancia);                     //Paso la distancia a una cadena para enviarla al visual
+  for (int i=0;i<str.length();i++)                          //Recorrido de la cadena
+ {
+ Serial.print(str.charAt(i));                               //Envío carácter a carácter
+ delay(50);
+ }
+ Serial.println();                               
+  
+  float frecuencia = -7.5 * distancia + 900;                //La frecuencia tiene una relación lineal con la distancia
+  float duracion = 5 * distancia + 200;                     //La duración también tiene una relación lineal con la distancia
   tone(BUZZER,frecuencia , duracion); 
 }
 //Estas relaciones lineales las hemos obtenido para que se ajusten a la capacidad de los sensores y a nuestras preferencias
  
 
- 
-void iniciarTrig() // Funcion que inicia la secuencia del Trigger
+// Funcion que inicia la secuencia del Trigger 
+void iniciarTrig()
 {
-  // Ponemos el Trigger en estado bajo y esperamos 2 ms
-  digitalWrite(TRIGGER, LOW);
+  
+  digitalWrite(TRIGGER, LOW);                              // Ponemos el Trigger en estado bajo y esperamos 2 ms
   delayMicroseconds(2);
  
-  // Ponemos el pin Trigger a estado alto y esperamos 10 ms
-  digitalWrite(TRIGGER, HIGH);
+  
+  digitalWrite(TRIGGER, HIGH);                             // Ponemos el pin Trigger a estado alto y esperamos 10 ms
   delayMicroseconds(10);
  
-  // Terminamos poniendo el pin Trigger en estado bajo 
-  digitalWrite(TRIGGER, LOW);
+ 
+  digitalWrite(TRIGGER, LOW);                              // Terminamos poniendo el pin Trigger en estado bajo
 }
 
 
-float ObtenerDist() // Funcion que devuelve una variable tipo float que contiene la distancia
+int ObtenerDist()                                          // Funcion que devuelve una variable tipo float que contiene la distancia
 {
-  unsigned long tiempo = pulseIn(ECHO, HIGH);//Mide el tiempo que tarda en recibir la onda el transductor ECHO
- 
- // Obtenemos la distancia en cm
-  float distancia = tiempo * 0.01715;  //La distancia es igual al tiempo por la velocidad del sonido en cm por microsegundo
- //dado que "pulse in" mide el tiempo en microsegundos
+  unsigned long tiempo = pulseIn(ECHO, HIGH);              //Mide el tiempo que tarda en recibir la onda el transductor ECHO
+  // Obtenemos la distancia en cm
+  int distancia = tiempo * 0.01715;                        //La distancia es igual al tiempo por la velocidad del sonido en cm por microsegundo
  
   return distancia;
 }
